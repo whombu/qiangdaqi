10c6ca344863c1e1df8e3e8aa2e16c78e9f8b6dc
whombu <buxiaoliang@126.com>
2018-10-15 23:12:22 +0800

    update index, version 1.0.6

--- page/index/index.js
+++ page/index/index.js
@@ -51,5 +51,25 @@ Page({
     wx.navigateTo({
       url: '../newRoom/index',
     });
+  },
+  onShareAppMessage: function (ops) {
+    var me = this;
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: qcloud.getSession().userInfo.nickName + '邀请你玩抢答比赛小程序',
+      path: '/page/index/index',
+      imageUrl: '/images/qiang.jpg',
+      success: function (res) {
+        // 转发成功 
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败 
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
   }
 });
